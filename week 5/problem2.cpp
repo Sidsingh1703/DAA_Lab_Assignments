6af952e6b7a29939a61ad6228c5fa6d0bef472f5
Sidsingh1703 <69680113+Sidsingh1703@users.noreply.github.com>
2021-08-20 10:05:58 +0530

    Update problem2.cpp

--- week 5/problem2.cpp
+++ week 5/problem2.cpp
@@ -1,1 +1,43 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+ 	int t;
+ 	cin>>t;
+ 	while(t--)
+ 	{
+ 		int n,key,l,r,f=0;
+ 		cin>>n;
+ 		int a[n];
+ 		for(int i=0;i<n;i++)
+ 		{
+ 			cin>>a[i];
+		}
+		cin>>key;
+		sort(a,a+n);
+		l=0;
+		r=n-1;
+		while(l<r)
+		{
+			if(a[l]+a[r]==key)
+			{
+				f=1;
+				cout<<a[l]<<" "<<a[r]<<" ";
+				l++;
+				r--;
+			}
+			else if(a[l]+a[r]>key)
+			{
+		        
+			    r--;
+		    }
+			else
+			l++;
+			
+		}
+		if(f==0)
+		{
+			cout<<"No such pair exists"<<endl;
+		}
+	}
+ }
